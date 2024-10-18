9823c565df9ca773dbdc6ab32fad8d41822a7f40
Christina Vieau <cvieau@live.com>
2024-10-17 23:07:12 -0400

    added link to create/edit Venue page

--- src/app/venues/edit/[id]/page.js
+++ src/app/venues/edit/[id]/page.js
@@ -1,7 +1,28 @@
-import React from 'react'
+'use client';
 
-export default function page() {
+import { useState, useEffect } from 'react';
+import VenueForm from '@/components/CreateVenueForm';
+import PropTypes from 'prop-types';
+import { getSingleVenue } from '../../../../api/venueData';
+
+export default function VenueEdit({ params }) {
+  const [editItem, setEditItem] = useState({});
+
+  const { firebaseKey } = params;
+
+  useEffect(() => {
+    getSingleVenue(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
+
+  // pass object to form
   return (
-    <div>page</div>
-  )
+    <>
+      <h1>Create/Edit Venue</h1>
+      <VenueForm obj={editItem} />
+    </>
+  );
 }
+
+VenueEdit.propTypes = {
+  params: PropTypes.objectOf({}).isRequired,
+};

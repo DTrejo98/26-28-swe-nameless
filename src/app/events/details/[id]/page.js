2dcefd2293c051bf8878afbcaaa7a71b1ad35235
David Trejo <trejo.m.david98@gmail.com>
2024-10-21 21:36:35 -0500

    event details page

--- src/app/events/details/[id]/page.js
+++ src/app/events/details/[id]/page.js
@@ -1,7 +1,36 @@
-import React from 'react'
+'use client';
 
-export default function page() {
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { getSingleEvent } from '../../../../api/eventData';
+
+// import { getSingleVenue } from '../../../../api/venueData';
+
+export default function ViewEventDetails({ params }) {
+  const [eventDetails, setEventDetails] = useState({});
+  // const [venueDetails, setVenueDetails] = useState([]);
+  const { id } = params;
+
+  useEffect(() => {
+    getSingleEvent(id).then(setEventDetails);
+    console.log(getSingleEvent(id));
+    // getSingleVenue(id).then(setVenueDetails);
+  }, [id]);
+
   return (
-    <div>page</div>
-  )
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="text-white ms-5 details">
+        <p>
+          Event Name
+          {eventDetails.artist}
+        </p>
+      </div>
+    </div>
+  );
 }
+
+ViewEventDetails.propTypes = {
+  params: PropTypes.objectOf({
+    date: PropTypes.string,
+  }).isRequired,
+};

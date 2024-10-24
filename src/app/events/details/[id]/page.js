6bc26808dadb3a437952fcfee2b02a0a977abdd3
David Trejo <trejo.m.david98@gmail.com>
2024-10-23 21:35:16 -0500

    Details page

--- src/app/events/details/[id]/page.js
+++ src/app/events/details/[id]/page.js
@@ -4,33 +4,43 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getSingleEvent } from '../../../../api/eventData';
 
-// import { getSingleVenue } from '../../../../api/venueData';
-
 export default function ViewEventDetails({ params }) {
   const [eventDetails, setEventDetails] = useState({});
-  // const [venueDetails, setVenueDetails] = useState([]);
   const { id } = params;
 
   useEffect(() => {
-    getSingleEvent(id).then(setEventDetails);
-    console.log(getSingleEvent(id));
-    // getSingleVenue(id).then(setVenueDetails);
+    getSingleEvent(id).then((data) => {
+      setEventDetails(data);
+    });
   }, [id]);
 
   return (
     <div className="mt-5 d-flex flex-wrap">
       <div className="text-white ms-5 details">
-        <p>
-          Event Name
-          {eventDetails.artist}
-        </p>
+        <h3>
+          {eventDetails.artist} at {eventDetails.venue?.name}
+          <ul>
+            <li>{eventDetails.date}</li>
+            <li>{eventDetails.venue?.address}</li>
+            <li>{eventDetails.venue?.city}</li>
+            <li>{eventDetails.venue?.state}</li>
+          </ul>
+        </h3>
       </div>
     </div>
   );
 }
 
 ViewEventDetails.propTypes = {
-  params: PropTypes.objectOf({
+  params: PropTypes.shape([]),
+  eventDetails: PropTypes.shape({
+    artist: PropTypes.string,
     date: PropTypes.string,
+    venue: PropTypes.shape({
+      name: PropTypes.string,
+      address: PropTypes.string,
+      city: PropTypes.string,
+      state: PropTypes.string,
+    }),
   }).isRequired,
 };

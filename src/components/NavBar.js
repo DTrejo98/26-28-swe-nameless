e7a5f59b4d856af6f39ac5ad1f0c5e13d294480b
David Trejo LT <trejo.m.david98@gmail.com>
2024-10-26 09:25:13 -0500

    Ticket Republic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>Master of Tickets</Navbar.Brand>
+          <Navbar.Brand>Ticket Republic</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
